4f59677cd23b23fa500e04271eb304d87193ec3a
yanchongwen <yanchongwen101@163.com>
2020-07-31 18:28:18 +0800

    ① 修改了一处小细节的命名。

--- sentry/init/demo-config.ts
+++ sentry/init/demo-config.ts
@@ -1,4 +1,4 @@
-export const Sentry_Cfg = {
+export const Demo_Sentry_Cfg = {
   crossOrigin      : 'anonymous',
   dns              : 'https://cc5039e47b3945e7bdfd1bce6a502db5@sentry.bgex.info/3',
   pureJsUrl        : 'https://browser.sentry-cdn.com/5.5.0/bundle.min.js',
@@ -11,7 +11,7 @@ declare global {
   // interface Window {
   //   Sentry_Cfg: typeof Sentry_Cfg;
   // }
-  type Sentry_Cfg_Type = typeof Sentry_Cfg;
+  type Sentry_Cfg_Type = typeof Demo_Sentry_Cfg;
 }
 
 // window.Sentry_Cfg = Sentry_Cfg;
