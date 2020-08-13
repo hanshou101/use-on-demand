00e6dc65561abb5a8a7e3f460fe70e549ecb0e1d
yanchongwen <yanchongwen101@163.com>
2020-08-13 12:29:14 +0800

    ① 添加了，两个【style处理】的方法。

--- dom/dom-style.ts
+++ dom/dom-style.ts
@@ -7,10 +7,11 @@ export class DomStyle_Helper {
 
   /**
    * 获取元素的css属性值
-   * @param ele dom元素
-   * @param cssAttribute css属性名称
    */
-  public static getDomStyle(ele: HTMLElement, cssAttribute: string) {
+  public static getDomStyle(
+    ele: HTMLElement,           // 元素
+    cssAttribute: string,       // 实时属性名
+  ) {
     if (!ele || !ele.nodeName) {
       console.error('ele 必须是一个dom元素');
       return;
@@ -32,7 +33,9 @@ export class DomStyle_Helper {
     }
   }
 
-  // 获取浏览器宽高
+  /**
+   * 【浏览器窗口】，内部内容区域，宽高
+   */
   public static getDocumentWidthHeight() {
     if (window.innerHeight != null) {
       return {
@@ -53,14 +56,16 @@ export class DomStyle_Helper {
   }
 
   /**
-   * 获取元素距浏览器最顶部及最左边的距离
-   * @param ele dom元素
+   * 获取元素距【浏览器文档最顶部、最左边】的距离
    */
-  public static get_AbsoluteOffset_Position(ele: HTMLElement): MyAbsolutePosition {
-    const position   = {
+  public static get_AbsoluteOffset_Position(
+    ele: HTMLElement,
+  ): MyAbsolutePosition {
+    const position: MyAbsolutePosition = {
       top : 0,
       left: 0,
     };
+
     let offsetParent = ele.offsetParent as HTMLElement;   // TODO offsetParent，为距离最近的【定位元素】。  （若不存在距离最近的【定位元素】，则直接获取  根元素（比如  <html>））
     position.top     = ele.offsetTop;
     position.left    = ele.offsetLeft;
@@ -73,18 +78,45 @@ export class DomStyle_Helper {
   }
 
   /**
+   *  获取以下距离：
+   *        1.【元素底部】，到【Window可见区域  底部】的距离
+   *        2.【元素右侧】，到【Window可见区域  右侧】的距离。
+   */
+  public static get_eB2wB_eR2wR(e: HTMLElement) {
+    // alert('尺寸改变，开始检测');
+    const domAttr = {
+      posit  : DomStyle_Helper.get_AbsoluteOffset_Position(e),
+      oHeight: e.offsetHeight,
+      oWidth : e.offsetWidth,
+    };
+    const wAttr   = {
+      visibleH: window.innerHeight,
+      visibleW: window.innerWidth,
+    };
+    console.log('属性信息', domAttr);
+    console.log('窗口信息', wAttr);
+    const res = {
+      eB2wB: wAttr.visibleH - domAttr.posit.top - domAttr.oHeight,  // 元素底部，到 Window可见区域 底部
+      eR2wR: wAttr.visibleW - domAttr.posit.left - domAttr.oWidth,  // 元素右侧，到 Window可见区域 右侧
+    };
+    console.log('距离下方、右方距离', res);
+    return res;
+  }
+
+  /**
    * 兼容性：获取浏览器滚动条距离顶部的位置
    */
   public static getScrollTop(): number {
     return window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || -1;
   }
 
+
   /**
    * 获取兄弟节点
-   * @param ele
-   * @returns {Array}
    */
-  public static getSiblingsDoms(ele: HTMLElement): Array<HTMLElement> {
+  public static getSiblingsDoms(
+    ele: HTMLElement,               // 目标元素
+  ): Array<HTMLElement> {
     if (ele.parentNode) {
       const a = [];
       const p = ele.parentNode.children;
@@ -102,12 +134,11 @@ export class DomStyle_Helper {
   /**
    * 兼容性：判断两个元素是否是包含关系。（无限层级）
    *        1.无限个层级，都有效，
-   *
-   * @param ele 父元素
-   * @param childEle 子元素
-   * @returns {Boolean}
    */
-  public static isContains_otherEle(ele: HTMLElement, _childEle: Node): boolean {
+  public static isContains_otherEle(
+    ele: HTMLElement,           // 父级元素
+    _childEle: Node,            // 子孙级元素
+  ): boolean {
     let childEle = _childEle;
     if (ele == childEle) {
       return false;
@@ -129,6 +160,22 @@ export class DomStyle_Helper {
         }
       }
     }
+  }
+
+
+  /**
+   * 修复，Android下【软键盘】弹出时，导致的【VH不准确】的问题。
+   */
+  public static fix_AndroidKeyboard_errorVH(
+    timeout: number = 300,
+  ) {
+    // 避免【小键盘】弹起，导致的【vh失效】。
+    setTimeout(function () {
+      const viewHeight = window.innerHeight;
+      const viewWidth  = window.innerWidth;
+      const viewport   = document.querySelector('meta[name=viewport]');
+      viewport?.setAttribute('content', 'height=' + viewHeight + 'px, width=' + viewWidth + 'px, initial-scale=1.0');
+    }, timeout);
   }
 
 
