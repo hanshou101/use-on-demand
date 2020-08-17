1688dfd1f45c23763bb37328ee2dc3808e578452
yanchongwen <yanchongwen101@163.com>
2020-08-17 11:38:31 +0800

    ① 添加了【时间日期】的一部分工具。

--- symbol-date-time/SDateTime_Helper.ts
+++ symbol-date-time/SDateTime_Helper.ts
@@ -1,15 +1,21 @@
+declare namespace CountDownNS {
+  interface StepRes {
+    day: string;
+    hours: string;
+    minutes: string;
+    seconds: string;
+  }                                       //
+  type StepCb = (res: StepRes, stopFn: Function) => void;
+}
+
 export class SDateTime_Helper {
   /**
    * 根据指定时间的倒计时功能
-   * // startTime 开始时间
-   * @param __endTime 结束时间
-   * @param step_callBack 倒计时每走一次的回调函数
-   * @param finish_Callback 倒计时结束回调函数
    */
   public countDownForTime(
-    __endTime: string,
-    step_callBack: Function,
-    finish_Callback: Function,
+    __endTime: string,          // 结束时间
+    step_callBack: CountDownNS.StepCb,                  // 倒计时每走一次的回调函数
+    finish_Callback: Function,  // 倒计时结束回调函数
   ): void {
     const timeReg = /\d{13,}/;
     if (!timeReg.test(__endTime)) {
@@ -22,37 +28,94 @@ export class SDateTime_Helper {
     }
     const endTime: number = Number(__endTime);
     const endDate         = new Date(endTime);
-    let distance          = 0;
-    const result          = {
-      day    : '0',
-      hours  : '0',
-      minutes: '0',
-      seconds: '0',
-    };
+    let ms                = 0;
     const clear           = function () {
       clearInterval(timer);
     };
     const timer           = setInterval(() => {
-      distance = (endDate.getTime() - new Date().getTime()) / 1000;
-      if (distance < 0) {
+      ms         = (endDate.getTime() - new Date().getTime());
+      let result = {
+        day    : '0',
+        hours  : '0',
+        minutes: '0',
+        seconds: '0',
+      };
+      if (ms < 0) {
         clearInterval(timer);
         if (finish_Callback && typeof finish_Callback == 'function') {
-          finish_Callback(result);
+          finish_Callback(result);      // 此时，都为零
         }
         return;
       }
-      const day      = Math.floor(distance / 60 / 60 / 24);
-      const hours    = Math.floor(distance / 60 / 60 % 24);
-      const minutes  = Math.floor(distance / 60 % 60);
-      const seconds  = Math.floor(distance % 60);
-      result.day     = day >= 10 ? day + '' : ('0' + day);
-      result.hours   = hours >= 10 ? hours + '' : ('0' + hours);
-      result.minutes = minutes >= 10 ? minutes + '' : ('0' + minutes);
-      result.seconds = seconds >= 10 ? seconds + '' : ('0' + seconds);
-      console.log(result);
-
+      result = SDateTime_Helper.getTimePartStr(ms);       // 这一条，可能是负数。
       step_callBack(result, clear);
     }, 1000);
+  }
+
+  public static getTimePartStr(
+    ms: number,       // 毫秒
+  ) {
+    const s       = ms / 1000;
+    const day     = Math.floor(s / 60 / 60 / 24);
+    const hours   = Math.floor(s / 60 / 60 % 24);
+    const minutes = Math.floor(s / 60 % 60);
+    const seconds = Math.floor(s % 60);
+
+    const result = {
+      day    : this.completeTimePartZero(day),
+      hours  : this.completeTimePartZero(hours),
+      minutes: this.completeTimePartZero(minutes),
+      seconds: this.completeTimePartZero(seconds),
+    };
+    console.log(result);
+    return result;
+  }
+
+  /**
+   * 将0-9的数字前面加上0
+   *        1.举例："1"，变为"01"。
+   */
+  public static completeTimePartZero(timePart: string | number) {
+    return parseInt(timePart) < 10 ?
+      '0' + String(timePart)
+      : String(timePart);
+  }
+
+}
+
+export class ServerTime_Helper {
+  /**
+   *  差异时间
+   *          1.【服务器】-【客户端】。
+   *          2.毫秒为单位。
+   */
+  public static diffTime = 0;
+
+  /**
+   *  保存【时间差值】
+   */
+  public static saveDiffTime(header: { Date: string }) {
+    const HeaderDate = header.Date;
+    const serverT    = new Date(HeaderDate).getTime();
+    const clientT    = new Date().getTime();
+    this.diffTime    = serverT - clientT;
+  }
+
+  /**
+   *  获取【服务器真实】当下时间，时间戳。
+   */
+  public static getServer_NowTime_Ts() {
+    return new Date().getTime() + this.diffTime;
+  }
+
+  /**
+   *  获取【服务器真实】目标时间，时间戳。
+   */
+  public static getServer_TargetTime_Ts(
+    targetTimeStr: string,         // 一般由服务器返回，形如【yyyy-MM-dd HH:mm:ss】
+  ) {
+    const fix_iOSBug_Str = targetTimeStr.replace(/-/g, '/');       // IOS的时间特质，有点奇怪。所以在这里，加上转化判断。
+    return new Date(fix_iOSBug_Str).getTime() + this.diffTime;
   }
 
 }
