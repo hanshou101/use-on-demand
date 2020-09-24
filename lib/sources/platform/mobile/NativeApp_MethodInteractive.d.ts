0601f002af61346ed3674d3af9b5e6e3164e985b
YanChongWen <yanchongwen101@163.com>
2020-09-24 12:21:41 +0800

    ① 修改了【原生APP调用】的【工具类】，添加了一个泛型T。(2)

--- lib/sources/platform/mobile/NativeApp_MethodInteractive.d.ts
+++ lib/sources/platform/mobile/NativeApp_MethodInteractive.d.ts
@@ -1,7 +1,7 @@
 export declare class xX_NativeApp_MethodInteractive {
     private static readonly _paramSeparator;
-    static invokeNativeMethod(methodName: string, // 方法名
-    ...params: Array<any>): Promise<any>;
+    static invokeNativeMethod<T = any>(methodName: string, // 方法名
+    ...params: Array<any>): Promise<T>;
     private static invoke_iosFn;
 }
 //# sourceMappingURL=NativeApp_MethodInteractive.d.ts.map
