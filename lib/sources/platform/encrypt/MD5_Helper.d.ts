7fee1f852b3edb3df68d8672fd16fdd4f02fc690
YanChongWen <yanchongwen101@163.com>
2020-09-28 11:05:54 +0800

    ① 添加了【MD5】和【远程拉取文件MD5】的【工具库】。(2)    （发现IDE有点傻叉，每次都给我遗留这些）

---
+++ lib/sources/platform/encrypt/MD5_Helper.d.ts
@@ -0,0 +1,14 @@
+export declare class xX_MD5_Helper {
+    static hash(str: string): string;
+    private static md5cycle;
+    static cmn(q: number, a: number, b: number, x: number, s: number, t: number): number;
+    private static ff;
+    private static gg;
+    private static hh;
+    private static ii;
+    private static md51;
+    private static md5blk;
+    private static rhex;
+    private static hex;
+}
+//# sourceMappingURL=MD5_Helper.d.ts.map
---
+++ lib/sources/platform/file-blob/FileMd5_Helper.d.ts
@@ -0,0 +1,5 @@
+export declare class xX_FileMd5_Helper {
+    static calculateMD5(blob: Blob): Promise<string>;
+    static downloadAndCreateHash(remoteUrl: string): Promise<string>;
+}
+//# sourceMappingURL=FileMd5_Helper.d.ts.map
