caf0482934f7dc972647d673ccd5b7878bc5a499
YanChongWen <yanchongwen101@163.com>
2020-09-29 12:09:14 +0800

    ① 添加了【Vue-Router】相关的【工具类】。

--- sources/ts/ts-types/router/Router.d.ts
+++ sources/ts/ts-types/router/Router.d.ts
@@ -31,8 +31,13 @@
 //   }
 // }
 
-import {Route} from 'vue-router';
+import Router, { Route, RawLocation } from 'vue-router';
 
 declare global {
-  type RouteType = Route;
+	type Route_Type = Route;
+	type Router_Type = typeof Router;
+	type RawLocation_Type = typeof RawLocation;
+
+	type Router_PushFn_A_Type = (location: RawLocation) => Promise<Route>;
+
 }
---
+++ sources/vue/router/VueRouter_Helper.ts
@@ -0,0 +1,19 @@
+export class VueRouter_Helper {
+	/**
+	 * 用于修复【navigation cancelled from / to /login with a new navigation】报错的问题。
+	 * 				1.直接原因：
+	 * 						1.【3.x】版本特有。
+	 * 						2.【vue-router@3.x】版本，如果路由地址跳转相同, 且没有捕获到错误，则会产生错误。
+	 * 				2.解决思路：
+	 * 						2.我们统一将【Router.prototype.push】方法，加上一个【catch】语句；以避免报错卡住。
+	 */
+	public static fix_V3_NavigationCancelled_Error(_router: Router_Type) {
+		const originalPush: Router_PushFn_A_Type = _router.prototype.push;
+		_router.prototype.push                   = function push(location: RawLocation_Type) {
+			return originalPush.call(this, location).catch(err => {
+				console.log('Router报错', err);
+				return err;
+			});
+		};
+	}
+}
