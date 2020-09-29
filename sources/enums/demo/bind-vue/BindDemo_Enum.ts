bb3f29958b3b16618fb36dc6b433d9ea98bb3f8d
YanChongWen <yanchongwen101@163.com>
2020-09-29 20:38:45 +0800

    ① 修改了一小处【枚举】的代码。

--- sources/enums/demo/bind-vue/BindDemo_Enum.ts
+++ sources/enums/demo/bind-vue/BindDemo_Enum.ts
@@ -1,18 +1,26 @@
 import Vue from 'vue';
 
 enum TestE {
-  A,
-  B,
+	A,
+	B,
 }
+
+const enum BetterEnumE {
+	AAA,
+	BBB,
+}
+
+// WARN 以下这种用法，对于【const enum】会报错。
+// console.log(BetterEnumE[0]);
 
 //
 
 const GlobalEnums = {
-  TestE: TestE,
+	TestE: TestE,
 };
 
 Vue.prototype.$Enums = GlobalEnums;
 
 declare global {
-  type GlobalEnums_Type = typeof GlobalEnums;
+	type GlobalEnums_Type = typeof GlobalEnums;
 }
