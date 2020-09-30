eed1ab89b8b101510586e32e0c2ee00a69d051a2
YanChongWen <yanchongwen101@163.com>
2020-09-30 12:56:25 +0800

    ① 增加了一处，日志的打印。

--- sources/vue/router/VueRouter_Helper.ts
+++ sources/vue/router/VueRouter_Helper.ts
@@ -8,6 +8,7 @@ export class VueRouter_Helper {
 	 * 						2.我们统一将【Router.prototype.push】方法，加上一个【catch】语句；以避免报错卡住。
 	 */
 	public static fix_V3_NavigationCancelled_Error(_router: Router_Type) {
+		console.error('此处，用于修复【Vue-Router-3】的【跳转Promise报错】，导致中断的Bug。');
 		const originalPush: Router_PushFn_A_Type = _router.prototype.push;
 		_router.prototype.push                   = function push(location: RawLocation_Type) {
 			return originalPush.call(this, location).catch(err => {
