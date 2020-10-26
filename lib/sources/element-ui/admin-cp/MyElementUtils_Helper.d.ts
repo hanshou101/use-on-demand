4d7235bef92cc5bd13b7db31422d8eff93f1a071
YanChongWen <yanchongwen101@163.com>
2020-10-26 11:27:20 +0800

    ① 提交了【依赖版本】的改变。

--- lib/sources/element-ui/admin-cp/MyElementUtils_Helper.d.ts
+++ lib/sources/element-ui/admin-cp/MyElementUtils_Helper.d.ts
@@ -1,4 +1,4 @@
 export declare class xX_MyElementUtils_Helper {
-    static get_ElTag_HtmlText(colorIndex: number, optionText: string): string;
+    static get_MyElTagLike_HtmlText(colorIndex: number, optionText: string): string;
 }
 //# sourceMappingURL=MyElementUtils_Helper.d.ts.map
--- sources/element-ui/admin-cp/MyElementUtils_Helper.ts
+++ sources/element-ui/admin-cp/MyElementUtils_Helper.ts
@@ -1,7 +1,7 @@
 import {xX_data_elTagColorFilter} from './MyElementUtils';
 
 export class xX_MyElementUtils_Helper {
-  public static get_ElTag_HtmlText(colorIndex: number, optionText: string) {
+  public static get_MyElTagLike_HtmlText(colorIndex: number, optionText: string) {
     return `<span class="el-tag el-tag--${xX_data_elTagColorFilter[colorIndex]}">${optionText}</span>`;
   }
 }
