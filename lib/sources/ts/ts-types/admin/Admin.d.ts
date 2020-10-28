ba31b91d8e452e34f0a9b7a1ecd4965e8a72faa4
YanChongWen <yanchongwen101@163.com>
2020-10-28 13:15:21 +0800

    ① 修正了一处类型声明。

--- lib/sources/ts/ts-types/admin/Admin.d.ts
+++ lib/sources/ts/ts-types/admin/Admin.d.ts
@@ -1,21 +1,29 @@
-	 interface PreUploadBean_Type {
-		dir?: string;
-		policy?: string;
-		signature?: string;
-		callback?: string;
-		accessid?: string;
-		host?: string;
-	}
+// 给业务库用
+export interface PreUploadBean {
+	dir?: string;
+	policy?: string;
+	signature?: string;
+	callback?: string;
+	accessid?: string;
+	host?: string;
+}
 
-	 interface OssUploadBean_Type {
-		name?: string;
-		key?: string;
-		policy?: string;
-		OSSAccessKeyId?: string;
-		success_action_status?: number;
-		callback?: string;
-		signature?: string;
-	}
+// 给业务库用
+export interface OssUploadBean {
+	name?: string;
+	key?: string;
+	policy?: string;
+	OSSAccessKeyId?: string;
+	success_action_status?: number;
+	callback?: string;
+	signature?: string;
+}
+
+declare global {
+	// 给自己用
+	type PreUploadBean_Type = PreUploadBean;
+	// 给自己用
+	type OssUploadBean_Type = OssUploadBean;
 
 	interface ExportExcelOption_Type {
 		url: string;
@@ -26,4 +34,4 @@
 		current?: string;
 		size?: string;
 	}
-
+}
