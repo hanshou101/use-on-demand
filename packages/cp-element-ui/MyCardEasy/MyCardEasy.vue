2ed056ac0477e6588419814680fc5d635c1b7b74
YanChongWen <yanchongwen101@163.com>
2020-10-29 21:32:05 +0800

    ① 重新补回了【MyCardEasy】中的【slot插槽】。（？？？之前的改动为什么没有了）

--- packages/cp-element-ui/MyCardEasy/MyCardEasy.vue
+++ packages/cp-element-ui/MyCardEasy/MyCardEasy.vue
@@ -8,6 +8,11 @@
 
 			<div slot="header">
 				<span>{{ cardItem.cardTitle }}</span>
+
+				<slot name="header">
+					<!-- 示例：<template v-slot:header> -->
+				</slot>
+
 			</div>
 			<div>
 				<template v-for="( rowArray , rowIndex ) in cardItem.rows ">
@@ -26,6 +31,11 @@
 						</el-col>
 					</el-row>
 				</template>
+
+				<slot name="body">
+					<!-- 示例：<template v-slot:body> -->
+				</slot>
+
 			</div>
 		</el-card>
 	</fragment>
