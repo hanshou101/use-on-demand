4d31336199b02366ab72fae8f9f4bed8f5d90a13
YanChongWen <yanchongwen101@163.com>
2021-01-05 20:07:15 +0800

    ① 封装了常见的【CRUD】功能，为其提供一个生成函数，使其更加抽象化。

--- sources/server/simple-server/src/routes/db.ts
+++ sources/server/simple-server/src/routes/db.ts
@@ -1,54 +1,90 @@
-import { Router, Request, Response, NextFunction } from 'express';
-import { LowDB_Helper }                            from '../base/lowdb/LowDB_Helper';
+import {Router, Request, Response, NextFunction} from 'express';
+import {LowDB_Helper}                            from '../base/lowdb/LowDB_Helper';
 
-const { write, read } = LowDB_Helper;
-const r_db            = Router();
+const {write, read} = LowDB_Helper;
+const r_db          = Router();
 
 write.db.init_whenEmpty({
-	projects: [{
-		id      : '213333333333',
-		content : '测试内容',
-		descTags: '1,2,3,4,5',
-		icon    : 'https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png',
-		name    : '董娜娜',
-	}],
+  projects : [{
+    id      : 'fc433541-6eb0-4e12-9faa-8b362a48aa74',
+    content : '测试内容',
+    descTags: '1,2,3,4,5',
+    icon    : 'https://gw.alipayobjects.com/zos/rmsportal/sBxjgqiuHMGRkIjqlQCd.png',
+    name    : '董娜娜',
+  }],
+  servers  : [],
+  siteMains: [],
 });
 
 // 测试
-r_db.get('/', function(req: Request, res: Response, next: NextFunction) {
-	res.send(read.db.getAll());
+r_db.get('/', function(req : Request, res : Response, next : NextFunction){
+  res.send(read.db.getAll());
 });
 
-// 获取项目列表
-r_db.get('/project', function(req: Request, res: Response, next: NextFunction) {
-	res.send(read.any.getValue('projects'));
-});
+function getTimeString(date : Date){
+  return new Date(date.valueOf() + 8 * 60 * 60 * 1000).toISOString().slice(0, 23 - 4).replace('T', ' ');
+}
 
-// 更新项目列表
-r_db.put('/project', function(req: Request, res: Response, next: NextFunction) {
-	const body: LowDB.Project = req.body;
-	write.array.updateSomeItem('projects', {
-		id: body.id,
-	}, {
-		...body,
-		updatedAt: new Date().valueOf() + '',
-	} as LowDB.Project);
-	console.log('获取数据', body);
-	res.send(body);
-});
+/**
+ * 快速生成CRUD。
+ */
+function crudRoute<BEAN extends {id : string}>(
+  r : Router,
+  apiDashPath : string,    // 需要以【/】开头
+  dbPath : string,
+){
+  // 获取列表
+  r.get(apiDashPath, function(req : Request, res : Response, next : NextFunction){
+    res.send(read.any.getValue(dbPath));
+  });
 
-// 创建项目列表
-r_db.post('/project', function(req: Request, res: Response, next: NextFunction) {
-	const body: LowDB.Project = req.body;
-	write.array.insertItem_withLodashId('projects', {
-		...body,
-		createdAt: new Date().valueOf() + '',
-		updatedAt: new Date().valueOf() + '',
-	} as LowDB.Project);
-	console.log('获取数据', body);
-	res.send(body);
-});
+  // 更新条目
+  r.put(apiDashPath, function(req : Request, res : Response, next : NextFunction){
+    const bean : BEAN = req.body;
+    write.array.updateSomeItem(dbPath, {
+      id: bean.id,
+    }, {
+      ...bean,
+      updatedAt: getTimeString(new Date()),
+    } as BEAN);
+    console.log('获取数据', bean);
+    res.send(bean);
+  });
+
+  // 创建条目
+  r.post(apiDashPath, function(req : Request, res : Response, next : NextFunction){
+    const bean : BEAN = req.body;
+    write.array.insertItem_withLodashId(dbPath, {
+      ...bean,
+      createdAt: getTimeString(new Date()),
+      updatedAt: getTimeString(new Date()),
+    } as BEAN);
+    console.log('获取数据', bean);
+    res.send(bean);
+  });
+
+  // 删除条目
+  r.delete(apiDashPath, function(req : Request, res : Response, next : NextFunction){
+    const bean : BEAN = req.body;
+    write.array.removeItem_byQuery(dbPath, {
+      id: bean.id,
+    });
+    console.log('获取数据', bean);
+    res.send(bean);
+  });
+
+}
+
+
+// 项目 ——————————————————————————————————————————————————————————————————————————————
+crudRoute<LowDB.Project>(r_db, '/project', 'projects');
+
+// 服务器 ——————————————————————————————————————————————————————————————————————————————
+crudRoute<LowDB.Server>(r_db, '/server', 'servers');
+
+crudRoute<LowDB.Server>(r_db, '/siteMain', 'siteMains');
+
 
 export {
-	r_db,
+  r_db,
 };
--- sources/ts/ts-types/lowdb/LowDB.d.ts
+++ sources/ts/ts-types/lowdb/LowDB.d.ts
@@ -1,18 +1,44 @@
-declare namespace LowDB {
-	interface Project {
-		id: string;            // 项目唯一ID
-		name: string;          // 项目名
-		icon: string;          // 图标（用默认值）
-		//
-		descTags: string;    // 描述标签
-		content: string;       // 介绍内容
-		//
-		createdAt?: string;    // 创建时间（？）
-		updatedAt?: string;    // 更新时间（？）
-	}
+declare namespace LowDB{
 
+  interface _Base{
+    //
+    createdAt? : string;    // 创建时间（？）
+    updatedAt? : string;    // 更新时间（？）
+  }
 
-	interface DB {
-		projects: Project[],
-	}
+  interface Project implements _Base{
+    id : string;            // 唯一ID
+    name : string;          // 项目名
+    icon : string;          // 图标（用默认值）
+    //
+    descTags : string;    // 描述标签
+    content : string;       // 介绍内容
+  }
+
+  interface Server implements _Base{
+    id : string;              // 唯一ID
+    projectId : string;       // 项目唯一ID
+    //
+    trueIP : string;          // 真实IP
+    systemType : string;      // 系统类型
+    systemVersion : string;   // 系统版本
+    openPort : string;        // 开放端口
+    webAppFirewall : string;  // 网页防火墙
+  }
+
+  interface SiteMain implements _Base{
+    id : string;              // 唯一ID
+    projectId : string;       // 项目唯一ID
+    //
+    cmsSystemInfo : string;         // CMS系统信息
+    cdnDetail : string;             // CDN详情
+    certDetail : string;            // 证书详情
+    dnsRecords : string;            // DNS导向记录
+  }
+
+  interface DB{
+    projects : Project[];
+    servers : Server[];
+    siteMains : SiteMain[];
+  }
 }
